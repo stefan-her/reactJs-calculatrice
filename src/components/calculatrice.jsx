b99d0a03e393a4ee3d0ed3810077813dc987a69e
Stefan Her <stefan.her@gmail.com>
2021-07-09 15:51:31 +0200

    formulaire calcul correction avec object dans le useState

--- src/components/calculatrice.jsx
+++ src/components/calculatrice.jsx
@@ -2,46 +2,41 @@ import React, { useState } from 'react';
 
 const Calculatrice = () => {
 
-    const [nb1, setNb1] = useState('');
-    const [nb2, setNb2] = useState('');
-    const [nb3, setNb3] = useState('');
+    const [nbs, setNbs] = useState({nb1 : '', nb2 : ''});
+    const [result, setResult] = useState('');
     const [op, setOp] = useState('+');
 
-    const handelChange = (nb, e) => {
-        const v = e.target.value;
-        if(/^([0-9]+)?$/.test(v)) {
-            if(nb === 1) { setNb1(v); }
-            if(nb === 2) { setNb2(v); }
+    const handelChange = (e) => {
+        const {value, name} = e.target;
+
+        if(true) {
+            setNbs({
+                ...nbs, //permet de ne pas perdre de valeur!!! c'est comme ça !!!
+                [name]:value
+            });
         }
     }
 
     const calcul = (e) => {
         e.preventDefault();
-        
-        const v1 = parseInt(nb1);
-        const v2 = parseInt(nb2);
-        let result = null;
 
-        console.log(op);
+        setResult(result => {
+            const v1 = parseFloat(nbs.nb1);
+            const v2 = parseFloat(nbs.nb2);
 
-        switch(true) {
-            case op === "+": result = v1 + v2;
-            break;
-            case op === "-": result = v1 - v2;
-            break;
-            case op === "*": result = v1 * v2;
-            break;
-            case op === "/": result = v1 / v2;
-            break;
-        }
-        console.log(result);
-        setNb3(result);
+            switch(op) {
+                case '+' : return v1 + v2;
+                case '-' : return v1 - v2;
+                case '*' : return v1 * v2;
+                case '/' : return (v2 !== 0) ? (v1 / v2) : "Division impossible";
+            }
+        });
     }
 
     return (
         <form onSubmit={calcul}>
             <label>nb1</label>
-            <input type="text" id="nb1" onChange={(e) => handelChange(1, e)} value={nb1} />
+            <input type="text" name="nb1" onChange={(e) => handelChange(e)} value={nbs.nb1} />
             <label>op</label>
             <select id="op" onChange={(e) => setOp(e.target.value)} value={op}>
                 <option value="+">+</option>
@@ -50,9 +45,9 @@ const Calculatrice = () => {
                 <option value="/">/</option>
             </select>
             <label>nb2</label>
-            <input type="text" id="nb2" onChange={(e) => handelChange(2, e)} value={nb2} />
+            <input type="text" name="nb2" onChange={(e) => handelChange(e)} value={nbs.nb2} />
             <input type="submit" value="calculer"/>
-            <input type="text" readOnly="readOnly" value={nb3}/>         
+            <input type="text" readOnly="readOnly" value={result}/>         
         </form>
     );
 }
